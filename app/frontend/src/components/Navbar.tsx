1ed6037d2cc661bf270614352858dcb7c5285c9c
wpellegrini <wpellegrini@gmail.com>
2026-04-06 16:39:21 -0300

    Update Navbar.tsx

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 /*import logoImg from "@/assets/logo-soulpellegrini-v2-small.png";*/
-const logoImg = "/assets/logo-soulpellegrini-v2-small.png";
+const logoImg = "*./assets/logo-soulpellegrini-v2-small.png";
 
 const navLinks = [
   { name: "Home", path: "/" },
